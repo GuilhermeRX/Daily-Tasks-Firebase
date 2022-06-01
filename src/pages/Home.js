bd50299198b18999bc049897d13a941a79954d29
Luiz Guilherme <guilhermerx18@gmail.com>
2022-06-01 08:00:04 -0300

    feat: add logic msg edit your task

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { BsListTask } from "react-icons/bs"
 import Form from '../Components/Form'
 import Header from '../Components/Header'
 import Table from '../Components/Table'
+import AppContext from '../context/AppContext'
 import { Main } from '../Style/Components/Main'
 import { DivHome } from '../Style/Pages/Home'
 export default function Home() {
+  const {taskInfo} = useContext(AppContext);
   return (
     <DivHome>
       <Header />
       <Main>
-        <h4>Add New Task</h4>
+        <h4>{taskInfo ? 'Edit Your Task ' : 'Add New Task'}</h4>
         <Form />
         <span>Your Tasks <BsListTask /></span>
         <Table />
