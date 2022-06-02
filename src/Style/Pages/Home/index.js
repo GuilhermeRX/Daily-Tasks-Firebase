ecf6db324f78c63b8062de1dbee8e3ad5c94eff3
Luiz Guilherme <guilhermerx18@gmail.com>
2022-06-02 14:08:35 -0300

    style: add height 100% on body e page home

--- src/Style/Pages/Home/index.js
+++ src/Style/Pages/Home/index.js
@@ -4,7 +4,7 @@ export const DivHome = styled.div`
   background: rgb(255,255,255);
   background: linear-gradient(170deg, rgba(255,255,255,1) 0%, rgba(220,233,246,1) 35%);
   width: 100%;
-  height: 100vh;
+  height: 100%;
   flex: 1;
   display: flex;
   flex-direction: column;
