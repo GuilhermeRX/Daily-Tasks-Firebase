9db1f6187906b43a43b192f809f2f32c79d02e98
Luiz Guilherme <guilhermerx18@gmail.com>
2022-06-06 12:07:45 -0300

    feat: start logic from review component

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -1,29 +1,40 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import AppContext from '../context/AppContext'
 import { GridOne, GridTwo, ReviewContainer } from '../Style/Components/Review'
 
 export default function Review() {
+  const {taskInfo} = useContext(AppContext)
+
+  const [done, setDone] = useState(0)
+  const [inProgress, setInProgress] = useState(0)
+  const [onGoing, setOnGoing] = useState(0)
+  const [waitReview, setWaitReview] = useState(0)
+
+  if(taskInfo) {
+
+  }
   return (
     <ReviewContainer>
       <GridOne>
         <div>
-          <h1>22</h1>
+          <h1>{done}</h1>
           <p>Done</p>
         </div>
         
         <div>
-          <h1>8</h1>
+          <h1>{inProgress}</h1>
           <p>In Progress</p>
         </div>
       </GridOne>
 
       <GridTwo>
       <div>
-          <h1>4</h1>
+          <h1>{onGoing}</h1>
           <p>Ongoing</p>
         </div>
         
         <div>
-          <h1>3</h1>
+          <h1>{waitReview}</h1>
           <p>Waiting for Review</p>
         </div>
       </GridTwo>
