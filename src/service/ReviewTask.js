42b32b162f754082657e8d232a8cce5fab7487aa
Luiz Guilherme <guilhermerx18@gmail.com>
2022-06-08 19:50:16 -0300

    feat: react toastify on task review

--- src/service/ReviewTask.js
+++ src/service/ReviewTask.js
@@ -1,11 +1,13 @@
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "./firebase";
+import { notifyReview } from "./Notify";
 
-const ReviewTask = async (id) => {
-  const docRef = doc(db, 'tasks', id);
+const ReviewTask = async (task) => {
+  const docRef = doc(db, 'tasks', task.id);
   await updateDoc(docRef, {
     status: 'review',
   })
+  notifyReview(task)
 }
 
 export default ReviewTask;
